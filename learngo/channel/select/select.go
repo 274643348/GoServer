0ce1644c2e513bf38890b017dfde7e9847e5e5df
XiaoYan <liujingyan@congmingpai.com>
2018-11-23 12:36:22 +0800

    增加注释

--- learngo/channel/select/select.go
+++ learngo/channel/select/select.go
@@ -7,9 +7,14 @@ import (
 
 /*
 经测试发现select是阻塞的
-1：如果case中的channel都都没有输入，则deadlock
+0：首先会执行所有的case XXX：中的XXX，等待看那个XXX县过来，
+1：如果case中的channel都都没有输入，则deadlock（可以通过default来防止卡死）
 2：如果有多个channel输入，随机任意一个
 3：如果完成一个channel后重写for循环，再次select
+
+select和time.after
+1:在每一次select中都会运行time.after创建新的channel time
+2:case <-time.after 和其他case相比，看谁谁快，如果同时，就select就随机选一个执行；
  */
 
 func sleep(seconds float32, endSignal chan<- bool) {
