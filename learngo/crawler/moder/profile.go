f1ae3f7e6668e149c7929463c9475f3e28619302
XiaoYan <liujingyan@congmingpai.com>
2018-11-25 21:16:33 +0800

    增加用户结构体

---
+++ learngo/crawler/moder/profile.go
@@ -0,0 +1,17 @@
+package moder
+
+type Profile struct {
+	Name string
+	Gender string
+	Age string
+	Height string
+	Weight string
+	Income string
+	Marrige string
+	Education string
+	Occupation string
+	Hukou string
+	Xingzuo string
+	House string
+	Car string
+}
