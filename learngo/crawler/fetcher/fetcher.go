50064112586fa13ccf5e0e3cbfc70d2e7bdecc56
XiaoYan <liujingyan@congmingpai.com>
2018-11-25 14:05:51 +0800

    独立fetcher翻译模块

---
+++ learngo/crawler/fetcher/fetcher.go
@@ -0,0 +1,51 @@
+package fetcher
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+
+//获取url中的数据（utf-8）
+func Fetcher(url string)([]byte,error){
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println()
+		return nil,fmt.Errorf("Error: status code %d", resp.StatusCode)
+	}
+
+	e := determineEncoding(resp.Body)
+	//借助gopm get -g -v golang.org/x/text中的transform.NewReader将编码转为utf-8
+	utf8Reader := transform.NewReader(resp.Body,e.NewDecoder())
+
+	//可以直接返回
+	return ioutil.ReadAll(utf8Reader)
+
+}
+/*
+ 借助gopm get -g -v golang.org/x/net/html
+ 中的charset.DetermineEncoding,来判断当前的内容是什么编码
+ */
+func determineEncoding(r io.Reader) encoding.Encoding{
+	bytes,err:=bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		fmt.Printf("Fetcher error %v",err)
+		//最好不要panic
+		return unicode.UTF8
+	}
+
+	e,_,_ := charset.DetermineEncoding(bytes,"")
+	return e
+}
--- learngo/crawler/main.go
+++ learngo/crawler/main.go
@@ -1,55 +1,21 @@
 package main
 
 import (
-	"bufio"
+
+	"./fetcher"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io"
-	"io/ioutil"
-	"net/http"
 	"regexp"
 )
 
 func main() {
-	resp, err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code %d", resp.StatusCode)
-		return
-	}
-
-	e := determineEncoding(resp.Body)
-	//借助gopm get -g -v golang.org/x/text中的transform.NewReader将编码转为utf-8
-	utf8Reader := transform.NewReader(resp.Body,e.NewDecoder())
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-
+	all,_ :=fetcher.Fetcher("http://www.zhenai.com/zhenghun")
 	printCityList(all)
 
 }
 
-/*
- 借助gopm get -g -v golang.org/x/net/html
- 中的charset.DetermineEncoding,来判断当前的内容是什么编码
- */
-func determineEncoding(r io.Reader) encoding.Encoding{
-	bytes,err:=bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
 
-	e,_,_ := charset.DetermineEncoding(bytes,"")
-	return e
-}
 
+//通过正则表达式获取城市和url列表
 func printCityList(contents []byte){
 	re :=regexp.MustCompile(`href=("http://www.zhenai.com/zhenghun/[a-zA-Z0-9]+")[^>]*>([^<]+)</a>`)
 	matchs := re.FindAllSubmatch(contents,-1)
