4112c453e749b9c81040b406fb1a7b6cb1974fcd
XiaoYan <liujingyan@congmingpai.com>
2018-11-27 10:23:36 +0800

    无限等待怎么版，就开协程嘛！就是爱你爱你。。。。

--- learngo/crawler/scheduler/scheduler.go
+++ learngo/crawler/scheduler/scheduler.go
@@ -9,11 +9,18 @@ type SimpleScheduler struct {
 }
 
 func ( e *SimpleScheduler) Submit(r engine.Request) {
-	//go func() {
-	//
-	//	e.workerChan<-r
-	//}()
-	e.workerChan<-r
+	//存在无限等待的可能，当受到out的输入时，不断的向scheduler中submit，导致worker被占满，
+	//然后继续向scheduler中submit，等待worker中in的接受，但是所有的worker都在工作，没有人接受，
+	//于是最初的out接受数据的逻辑就等待在了submit
+	//当任意一个worker中的逻辑做完后，调用out<-result时，由于上一个<-out在等待无法接受，所以out<-result也在等待，就这样爱你爱你。。。
+
+	// e.workerChan<-r
+
+	//方案一：将e.workerChan<-r丢到一个新的goroutine中，防止submit一直等待倒是双向等待；
+	go func() {
+
+		e.workerChan<-r
+	}()
 }
 
 //配置输入chan到sheduler中
