459a8312c7184eb66286064750a75f67d417ccc6
XiaoYan <liujingyan@congmingpai.com>
2018-11-30 21:22:31 +0800

    修改文件访问路径,将整个frontend放在服务器上,启动webServer.exe运行

--- learngo/crawler/frontend/starter.go
+++ learngo/crawler/frontend/starter.go
@@ -13,13 +13,15 @@ func main() {
 	//Age:([20 TO 30])
 
 	//防止找不到css文件
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./crawler/frontend/view"))))
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./view"))))
 
-	http.Handle("/search", controller.CreateSearchResultHandler("./crawler/frontend/view/template.html"))
+	http.Handle("/search", controller.CreateSearchResultHandler("./view/template.html"))
 
 	err := http.ListenAndServe(":9999", nil)
 
 	if err != nil {
 		panic(err)
 	}
+
+	//$ CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o webServe.exe starter.go
 }
