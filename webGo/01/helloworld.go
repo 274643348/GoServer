816095a11da2072f35722c53470a153c9c33dcd2
XiaoYan <liujingyan@congmingpai.com>
2018-12-14 16:10:00 +0800

    变量的使用

--- webGo/01/helloworld.go
+++ webGo/01/helloworld.go
@@ -1,7 +1,42 @@
 package main
 
-import "learngo/GoServer/webGo/01/say"
+import (
+	"fmt"
+	"learngo/GoServer/webGo/01/say"
+)
+
+var (
+	x = 1;
+	q = 2;
+)
 
 func main() {
 	diffName.DiffName();
+
+	fmt.Println(x,q);
+	//变量使用1
+	var a int;
+	a = 10;
+	fmt.Println(a);
+
+	//变量使用2
+	var b, c, d int;
+	b = 10;
+	c = 10;
+	d = 10;
+	fmt.Println(b, c, d);
+
+	//变量使用3
+	var e, f, g int = 10, 10, 10;
+	fmt.Println(e, f, g);
+
+	//简化版赋值(这种形式只能在函数中使用,在函数外部是不行)
+	h, i, j := 10, 10, 10;
+	fmt.Println(h, i, j);
+
+	//神奇的"_"
+	_, k := 10, 10;
+	fmt.Println("第一个10将会被丢弃", k);
+
+	//声明的变量必须被使用,否则编译报错
 }
--- webGo/01/say/say.go
+++ webGo/01/say/say.go
@@ -3,5 +3,5 @@ package diffName
 import "fmt"
 
 func DiffName(){
-	fmt.Print("包名可以跟文件名不同，但是一个文件下只能有一个包名（多个go文件，但package不一样）")
+	fmt.Println("包名可以跟文件名不同，但是一个文件下只能有一个包名（多个go文件，但package不一样）")
 }
