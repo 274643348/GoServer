68273ecbd5fda486790bde12f781f15d7e166c14
XiaoYan <liujingyan@congmingpai.com>
2018-12-26 14:15:06 +0800

    尝试使用beego,输出hello world

--- bokeProject/main.go
+++ bokeProject/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "github.com/astaxie/beego"
+
+type HomeController struct {
+	beego.Controller
+}
+
+func (this *HomeController) Get(){
+	this.Ctx.WriteString("hello world")
+}
 func main() {
-	
+	//注册路由
+	beego.Router("/",&HomeController{})
+	beego.Run()
 }
