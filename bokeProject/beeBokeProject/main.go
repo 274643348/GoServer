4161e97608f78649b758d650bda00f699003b318
XiaoYan <liujingyan@congmingpai.com>
2018-12-27 10:09:25 +0800

    增加ORM相关代码;

--- bokeProject/beeBokeProject/main.go
+++ bokeProject/beeBokeProject/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"github.com/astaxie/beego/orm"
+	"learngo/GoServer/bokeProject/beeBokeProject/models"
 	_ "learngo/GoServer/bokeProject/beeBokeProject/routers"
 	"github.com/astaxie/beego"
 )
 
+func init(){
+	models.RegisterDB()
+}
+
 func main() {
+	//打印orm信息
+	orm.Debug = true;
+
+	//自动建表
+	orm.RunSyncdb("default",false,true);
+
+
 	beego.Run()
 }
 
