d8945d2d72b1bda5347adba24cc85932cb9dd77f
XiaoYan <liujingyan@congmingpai.com>
2018-12-29 16:29:27 +0800

    中文解码

--- bokeProject/beeBokeProject/controllers/attach.go
+++ bokeProject/beeBokeProject/controllers/attach.go
@@ -12,6 +12,9 @@ type AttachController struct {
 }
 
 func(this * AttachController)Get(){
+	//href 中含有中文
+	// 当template 模版解析数据时， {{.}} 假设 被  "你好.txt"  给取代， 当在浏览器显示源码时，href=%e4%bd%a0%e5%a5%bd ，被自动编码
+	//url.QueryUnescape（） 进行解码。  这样就能在后台显示对应的中文路径或者资源
 	filePath,err := url.QueryUnescape(this.Ctx.Request.RequestURI[1:])
 	if err != nil {
 		this.Ctx.WriteString(err.Error())
